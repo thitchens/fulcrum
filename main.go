694de453794717927cd612e6d034fc2230d1e5be
afeeblechild <afeeblechild@gmail.com>
2023-03-10 21:57:03 -0800

    Add zap as logger

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/afeeblechild/fulcrum/lib"
+	"go.uber.org/zap"
 )
 
 type Configuration struct {
@@ -18,11 +19,15 @@ type Configuration struct {
 	Static       string
 }
 
-var config Configuration
-var logger *log.Logger
+var (
+	config *Configuration
+	logger *zap.Logger
+)
 
 func main() {
-	lib.P("iam-manager", lib.Version(), "started at", config.Address)
+	defer logger.Sync()
+
+	fmt.Println("fulcrum", lib.Version(), "started at", config.Address)
 
 	// handle static assets
 	mux := http.NewServeMux()
@@ -43,38 +48,49 @@ func main() {
 
 func init() {
 	loadConfig()
-	//TODO add check for logging functions to check if the file still exists
-	file, err := os.OpenFile("fulcrum.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	var err error
+	logger, err = buildLogger()
 	if err != nil {
-		log.Fatalln("Failed to open log file", err)
+		panic(err)
 	}
-	logger = log.New(file, "INFO ", log.Ldate|log.Ltime)
 }
 
-func loadConfig() {
+func buildLogger() (*zap.Logger, error) {
+	cfg := zap.NewProductionConfig()
+	cfg.OutputPaths = []string{"fulcrum.log", "stderr"}
+	return cfg.Build()
+}
+
+func loadConfig() (*Configuration, error) {
 	file, err := os.Open("config.json")
 
 	if err != nil {
-		log.Fatalln("Cannot open config file", err)
+		return nil, fmt.Errorf("cannot open config file: %v", err)
 	}
 
 	decoder := json.NewDecoder(file)
-	config = Configuration{}
-	err = decoder.Decode(&config)
+	config = &Configuration{}
+	err = decoder.Decode(config)
 
 	if err != nil {
-		log.Fatalln("Cannot get configuration from file", err)
+		return nil, fmt.Errorf("cannot get configuration from file: %v", err)
 	}
+
+	return config, err
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("web/templates/index.html")
+	rootTemplate := "web/templates/index.html"
+	t, err := template.ParseFiles(rootTemplate)
 	if err != nil {
-		// TODO update panic
-		panic(err)
+		logger.Error("cannot parse root template",
+			zap.String("error", err.Error()),
+		)
 	}
 	err = t.Execute(w, r)
 	if err != nil {
-		panic(err)
+		logger.Error("cannot execute root template",
+			zap.String("error", err.Error()),
+		)
 	}
 }
